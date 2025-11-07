aa7a9f590a6079011313b3dd066e38725f07d94a
akkiravolkov@gmail.com <Danchikxd05@gmail.com>
2025-11-07 19:37:39 +0300

    аааааааааа

--- django/Живой сайт/приложение/admin.py
+++ django/Живой сайт/приложение/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import *
+class ОтзывыТегиИнлайн(admin.TabularInline):
+    model = ОтзывыТеги
 
-admin.site.register([Отзыв, Аватарка])
+class ОтзывАдмин(admin.ModelAdmin):
+    inlines = [ОтзывыТегиИнлайн]
+
+admin.site.register(Отзыв, ОтзывАдмин)
+admin.site.register([Аватарка, Тег])
--- django/Живой сайт/приложение/models.py
+++ django/Живой сайт/приложение/models.py
@@ -8,8 +8,19 @@ from django.contrib.auth import get_user_model
 # Потом этого юзера будем привязывать в качестве свойства для других моделей
 
 # Каждая модель (каждый класс) наследуются от стандартного класса джанго (принимает его стандартные свойства).
+class Тег(models.Model): 
+    текст = models.TextField()
+
+    class Meta:
+        verbose_name = "Тег"
+        verbose_name_plural = "Теги"
+        
+    def __str__(self):
+        return self.текст
+
 class Отзыв(models.Model):
     автор = models.ForeignKey(Юзер, on_delete=models.CASCADE)
+
     # Каждая переменная класса - это название поля (колонки таблицы)
     дата_поста = models.DateTimeField(
         # Это полеавтоматически считывает время компьютера и присваивает его в качестве значения в момент создания записи в базе данных (БД)
@@ -22,6 +33,8 @@ class Отзыв(models.Model):
     )
 
     текст = models.TextField()
+
+    тег = models.ManyToManyField(Тег, through="ОтзывыТеги", blank=True)
 
     class Meta:
         verbose_name = "Отзыв"
@@ -37,9 +50,18 @@ class Аватарка(models.Model):
     картинка = models.ImageField(
         upload_to="аватарки"
     )
+
     class Meta:
         verbose_name = "Аватарка"
         verbose_name_plural = "Аватарки"
     
+
     def __str__(self):
         return self.юзер.username
+    
+
+
+
+class ОтзывыТеги(models.Model):
+    отзыв = models.ForeignKey(Отзыв, on_delete=models.CASCADE)
+    тег = models.ForeignKey(Тег, on_delete=models.CASCADE)
